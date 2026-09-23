0e8e172645c8c2ca6397b5db9dd4377c6ae54f91
Alca <f.hfelinaldunate@uandresbello.edu>
2026-09-23 01:56:54 -0300

    Primera parte del proyecto

---
+++ vetconnect-react/src/App.jsx
@@ -0,0 +1,61 @@
+import "./App.css";
+import Header from "./components/Header";
+import FormMascota from "./components/FormMascota";
+import { useState } from "react";
+import MascotaCard from "./components/MascotaCard";
+export default function App() {
+
+
+  function registrarMascota(nuevaMascota) {
+  <FormMascota
+  onRegistrar={registrarMascota}
+  />
+  const mascotaConId = {
+    id: Date.now(),
+    ...nuevaMascota
+  };
+  setMascotas([
+    ...mascotas,
+    mascotaConId
+  ]);
+}
+
+  const [mascotas, setMascotas] = useState([
+    {
+      id: 1,
+      nombre: "Firulais",
+      especie: "Perro",
+      edad: 5,
+      vacunada: true
+    },
+    {
+      id: 2,
+      nombre: "Michi",
+      especie: "Gato",
+      edad: 3,
+      vacunada: false
+    }
+  ]);
+
+  
+
+return(
+  <>
+  <Header />
+  <main className="contenedor">
+    <h2>Mascotas</h2>
+    <section className="lista-mascotas">
+      {mascotas.map((mascota) => (
+        <MascotaCard
+        key={mascota.id}
+        nombre={mascota.nombre}
+        especie={mascota.especie}
+        edad={mascota.edad}
+        vacunada={mascota.vacunada}
+        />
+        ))}
+    </section>
+  </main>
+  </>
+  );
+}
